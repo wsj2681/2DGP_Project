1637ae807f9e60c6bbbd525bb9da05b3daa73419
Woo_Sj <wsj94472681@gmail.com>
2019-10-13 19:13:10 +0900

    자유낙하 구현

---
+++ Sample.py
@@ -0,0 +1,38 @@
+from pico2d import *
+import time
+
+open_canvas(800, 600)
+bird = load_image('bird.png')
+grass = load_image('grass.png')
+x = 400
+y = 600
+g = 9.8
+vel = 5
+start = time.time()
+delay(1)
+end = time.time()
+etime = end - start
+
+
+def free():
+    global x, y
+    global end, start, etime
+
+    y -= (vel*etime + 1/2*g*etime**2)
+
+    end = time.time()
+
+    etime = end - start
+    if y < 100:
+        start = end
+while True:
+    clear_canvas()
+    grass.draw(400, 30)
+    bird.draw(x, y, 100, 100)
+    update_canvas()
+    delay(0.01)
+    if y < 100:
+        y = 600
+    free()
+
+close_canvas()
