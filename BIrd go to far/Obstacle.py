57ab6f438be9f1bd8d3f31bbb56d3533de57a293
Woo_Sj <wsj94472681@gmail.com>
2019-11-26 01:37:32 +0900

    sprite fix

--- BIrd go to far/Obstacle.py
+++ BIrd go to far/Obstacle.py
@@ -4,28 +4,28 @@ import random
 
 TIME_PER_ACTION = 0.5
 ACTION_PER_TIME = 1.0 / TIME_PER_ACTION
-FRAMES_PER_ACTION = 5
+FRAMES_PER_ACTION = 10
 
 class Obstacle:
     image = None
 
     def __init__(self):
-        self.x, self.y = random.randint(1000, 10000), random.randint(50, 550)
-        self.random_x = random.randint(200, 500)
+        self.x, self.y = random.randint(1000, 10000), random.randint(125, 550)
+        self.random_x = random.randint(100, 400)
         self.frame = 0
         if Obstacle.image is None:
-            Obstacle.image = load_image('Images/Obstacle_right.png')
+            Obstacle.image = load_image('Images/test.png')
 
     def get_bb(self):
-        return self.x - 25, self.y - 25, self.x + 25, self.y + 25
+        return self.x - 20, self.y - 20, self.x + 20, self.y + 19
 
     def update(self):
-        self.frame = (self.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % 5
+        self.frame = (self.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % 10
         self.x -= self.random_x * game_framework.frame_time
         if self.x < 0:
             self.x = random.randint(1000, 10000)
 
     def draw(self):
-        self.image.clip_draw(int(self.frame) * (40-3), 0, 35, 48, self.x, self.y)
+        self.image.clip_draw(int(self.frame) * 40, 0, 40, 48, self.x, self.y)
         draw_rectangle(*self.get_bb())
 
